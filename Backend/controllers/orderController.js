962fa780bf93ad927bb4a5b4b37fdf35667f564b
Nishu Priya <priyanishu0503@gmail.com>
2025-02-12 22:49:10 +0530

    style me

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -8,10 +8,7 @@ import Stripe from "stripe";
 const currency = "inr";
 const deliveryCharge = 10;
 
-const stripe = new Stripe(
-  process.env.STRIPE_SECRET_KEY ||
-    "sk_test_51POgPm05jhqmd2ImXGsGIzJRIH6xoaW9TWKMEoNoWQ0JQq5gzvyRhDi8VZaYBkzME4wVeDEN5zSAsfbvg2SgNRpW00kzhB0X9s"
-);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 /* const razorpayInstance = new razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
