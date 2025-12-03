7c0ad59e6143396298163a367120bfb55a25bdd1
Nishu Priya <priyanishu0503@gmail.com>
2025-12-03 11:51:27 +0530

    cors update

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,20 @@ connectCloudinary();
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:3000",
+      "https://style-me-frontend.vercel.app",
+      "https://style-me-admin.vercel.app",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // api endpoints
 app.use("/api/user", userRouter);
@@ -28,6 +41,5 @@ app.use("/api/order", orderRouter);
 app.get("/", (req, res) => {
   res.send("API Working");
 });
-
 
 app.listen(port, () => console.log("Server started on PORT: " + port));
